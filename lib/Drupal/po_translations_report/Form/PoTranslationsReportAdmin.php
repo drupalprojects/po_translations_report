bfaa75e924736b2d26181d5c82510be59093953d
izus <abouljamal.ismaeil@gmail.com>
2014-04-26 04:16:44 +0200

    Add console generated setting form.

---
+++ lib/Drupal/po_translations_report/Form/PoTranslationsReportAdmin.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\po_translations_report\Form\PoTranslationsReportAdmin.
+ */
+
+namespace Drupal\po_translations_report\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+
+class PoTranslationsReportAdmin extends ConfigFormBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormID()
+  {
+    return 'potranslationsreportadmin_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, array &$form_state)
+  {
+    $config = $this->config('po_translations_report.potranslationsreportadmin_config');
+    $form['folder_path'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Folder path'),
+      '#description' => $this->t(''),
+      '#default_value' => $config->get('folder_path')
+    );
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, array &$form_state)
+  {
+    return parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, array &$form_state)
+  {
+    parent::submitForm($form, $form_state);
+
+    $this->config('po_translations_report.potranslationsreportadmin_config')
+          ->set('folder_path', $form_state['values']['folder_path'])
+        ->save();
+  }
+}
