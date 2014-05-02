091b22fbde57ddb7409758a2e24134299d704c34
izus <abouljamal.ismaeil@gmail.com>
2014-05-02 21:20:54 +0200

    Handle the case where no po was found.

--- lib/Drupal/po_translations_report/Controller/DefaultController.php
+++ lib/Drupal/po_translations_report/Controller/DefaultController.php
@@ -12,11 +12,20 @@ class DefaultController extends ControllerBase {
    */
   public function content() {
     $config = \Drupal::config('po_translations_report.admin_config');
-    $iterator = new \DirectoryIterator($config->get('folder_path'));
-    foreach ($iterator as $fileinfo) {
+    $folder_path = $config->get('folder_path');
+    $folder = new \DirectoryIterator($folder_path);
+    $po_found = FALSE;
+    foreach ($folder as $fileinfo) {
       if ($fileinfo->isFile() && $fileinfo->getExtension() == 'po') {
+        // Flag we found at least one po file in this directory.
+        $po_found = TRUE;
         //dpm($fileinfo->getFilename());
       }
+    }
+    // Handle the case where no po file could be found in the provided path.
+    if(!$po_found){
+      $message = t('No po was found in %folder', array('%folder' => $folder_path));
+      drupal_set_message($message, 'warning');
     }
   }
 
