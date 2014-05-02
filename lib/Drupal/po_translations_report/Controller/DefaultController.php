d6f78952c25cc517b7e07ba4dcd6f34391b12f91
izus <abouljamal.ismaeil@gmail.com>
2014-05-02 21:05:21 +0200

    Handle po files discovering.

--- lib/Drupal/po_translations_report/Controller/DefaultController.php
+++ lib/Drupal/po_translations_report/Controller/DefaultController.php
@@ -4,15 +4,20 @@ namespace Drupal\po_translations_report\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-class DefaultController extends ControllerBase 
-{
+class DefaultController extends ControllerBase {
 
   /**
    * content
    * @return string
    */
   public function content() {
-    $config = $this->config('po_translations_report.admin_config');
-    return $config->get('folder_path');
+    $config = \Drupal::config('po_translations_report.admin_config');
+    $iterator = new \DirectoryIterator($config->get('folder_path'));
+    foreach ($iterator as $fileinfo) {
+      if ($fileinfo->isFile() && $fileinfo->getExtension() == 'po') {
+        //dpm($fileinfo->getFilename());
+      }
+    }
   }
+
 }
