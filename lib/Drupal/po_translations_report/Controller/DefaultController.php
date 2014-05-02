74b714a461b57e9c71ab987d6f11cf47c571e62d
izus <abouljamal.ismaeil@gmail.com>
2014-05-02 21:48:15 +0200

    Use PoStreamReader to read po files.

--- lib/Drupal/po_translations_report/Controller/DefaultController.php
+++ lib/Drupal/po_translations_report/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Drupal\po_translations_report\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Component\Gettext\PoStreamReader;
 
 class DefaultController extends ControllerBase {
 
@@ -19,11 +20,29 @@ class DefaultController extends ControllerBase {
       if ($fileinfo->isFile() && $fileinfo->getExtension() == 'po') {
         // Flag we found at least one po file in this directory.
         $po_found = TRUE;
-        //dpm($fileinfo->getFilename());
+        // Instantiate and initialize the stream reader for this file.
+        $reader = new PoStreamReader();
+        $reader->setURI($fileinfo->getRealPath());
+
+        try {
+          $reader->open();
+        } catch (\Exception $exception) {
+          throw $exception;
+        }
+
+        $header = $reader->getHeader();
+        if (!$header) {
+          throw new \Exception('Missing or malformed header.');
+        }
+        while ($item = $reader->readItem()) {
+          //dpm($item->getSource());
+          //dpm($item->getTranslation());
+        }
       }
     }
+
     // Handle the case where no po file could be found in the provided path.
-    if(!$po_found){
+    if (!$po_found) {
       $message = t('No po was found in %folder', array('%folder' => $folder_path));
       drupal_set_message($message, 'warning');
     }
