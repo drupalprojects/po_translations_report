ee599975c7b631c34700b2f883087710fe84739c
izus <abouljamal.ismaeil@gmail.com>
2014-05-03 01:43:19 +0200

    Use private variables and factorise code.

--- lib/Drupal/po_translations_report/Controller/DefaultController.php
+++ lib/Drupal/po_translations_report/Controller/DefaultController.php
@@ -7,6 +7,11 @@ use Drupal\Component\Gettext\PoStreamReader;
 
 class DefaultController extends ControllerBase {
 
+  private $translated_count = 0;
+  private $untranslated_count = 0;
+  private $not_allowed_translation_count = 0;
+  private $total_count = 0;
+
   /**
    * content
    * @return string
@@ -17,7 +22,7 @@ class DefaultController extends ControllerBase {
     $folder = new \DirectoryIterator($folder_path);
     $po_found = FALSE;
     $file_name = '';
-    $translated_count = $untranslated_count = $not_allowed_translation_count = $total_count = 0;
+    //$translated_count = $untranslated_count = $not_allowed_translation_count = $total_count = 0;
     $display = t('File name') . '__' . t('translated') . '__' . t('Untranslated') . '__' . t('not_allowed translation') . '__' . t('Total');
     foreach ($folder as $fileinfo) {
       if ($fileinfo->isFile() && $fileinfo->getExtension() == 'po') {
@@ -40,42 +45,43 @@ class DefaultController extends ControllerBase {
         while ($item = $reader->readItem()) {
           if (!$item->isPlural()) {
             $file_name = $fileinfo->getFilename();
-            if (locale_string_is_safe($item->getTranslation())) {
-              if ($item->getTranslation() != '') {
-                $translated_count++;
-              }
-              else {
-                $untranslated_count++;
-              }
-            }
-            else {
-              $not_allowed_translation_count++;
-            }
-            $total_count++;
+            $this->translationReport($item->getTranslation());
+//            if (locale_string_is_safe($item->getTranslation())) {
+//              if ($item->getTranslation() != '') {
+//                $translated_count++;
+//              }
+//              else {
+//                $untranslated_count++;
+//              }
+//            }
+//            else {
+//              $not_allowed_translation_count++;
+//            }
+//            $total_count++;
           }
           else {
             // Plural case.
             $plural = $item->getTranslation();
             foreach ($item->getSource() as $key => $source) {
-              if (locale_string_is_safe($plural[$key])) {
-                if ($plural[$key] != '') {
-                  $translated_count++;
-                }
-                else {
-                  $untranslated_count++;
-                }
-              }
-              else {
-                $not_allowed_translation_count++;
-              }
-              $total_count++;
+
+              $this->translationReport($plural[$key]);
+//              if (locale_string_is_safe($plural[$key])) {
+//                if ($plural[$key] != '') {
+//                  $translated_count++;
+//                }
+//                else {
+//                  $untranslated_count++;
+//                }
+//              }
+//              else {
+//                $not_allowed_translation_count++;
+//              }
+//              $total_count++;
             }
-            //dpm($item->getSource());
-            //dpm($item->getTranslation());
           }
         }
         $display .= '<br />';
-        $display .= $file_name . '__' . $translated_count . '__' . $untranslated_count . '__' . $not_allowed_translation_count . '__' . $total_count;
+        $display .= $file_name . '__' . $this->translated_count . '__' . $this->untranslated_count . '__' . $this->not_allowed_translation_count . '__' . $this->total_count;
       }
 
       // Handle the case where no po file could be found in the provided path.
@@ -86,6 +92,22 @@ class DefaultController extends ControllerBase {
     }
     // Display the results.
     return $display;
+  }
+
+  public function translationReport($translation) {
+
+    if (locale_string_is_safe($translation)) {
+      if ($translation != '') {
+        $this->translated_count++;
+      }
+      else {
+        $this->untranslated_count++;
+      }
+    }
+    else {
+      $this->not_allowed_translation_count++;
+    }
+    $this->total_count++;
   }
 
 }
