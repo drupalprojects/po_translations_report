d7e29391d214ba7b3349a56af330494795d6e062
izus <abouljamal.ismaeil@gmail.com>
2014-05-02 21:41:24 +0200

    Handle setting form validation is_dir and is_readable.

--- lib/Drupal/po_translations_report/Form/PoTranslationsReportAdmin.php
+++ lib/Drupal/po_translations_report/Form/PoTranslationsReportAdmin.php
@@ -9,22 +9,19 @@ namespace Drupal\po_translations_report\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 
-class PoTranslationsReportAdmin extends ConfigFormBase
-{
+class PoTranslationsReportAdmin extends ConfigFormBase {
 
   /**
    * {@inheritdoc}
    */
-  public function getFormID()
-  {
+  public function getFormID() {
     return 'potranslationsreportadmin_form';
   }
 
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, array &$form_state)
-  {
+  public function buildForm(array $form, array &$form_state) {
     $config = $this->config('po_translations_report.admin_config');
     $form['folder_path'] = array(
       '#type' => 'textfield',
@@ -38,20 +35,29 @@ class PoTranslationsReportAdmin extends ConfigFormBase
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, array &$form_state)
-  {
-    return parent::validateForm($form, $form_state);
+  public function validateForm(array &$form, array &$form_state) {
+    parent::validateForm($form, $form_state);
+    // Check if the path is for valid readable folder.
+    $folder_path = $form_state['values']['folder_path'];
+    if (!is_dir($folder_path)) {
+      $this->setFormError('folder_path', $form_state, $this->t('%folder_path is not a directory.', array('%folder_path' => $folder_path)));
+    }
+    else {
+      if (!is_readable($folder_path)) {
+        $this->setFormError('folder_path', $form_state, $this->t('%folder_path is not a readable directory.', array('%folder_path' => $folder_path)));
+      }
+    }
   }
 
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, array &$form_state)
-  {
+  public function submitForm(array &$form, array &$form_state) {
     parent::submitForm($form, $form_state);
 
     $this->config('po_translations_report.admin_config')
-          ->set('folder_path', $form_state['values']['folder_path'])
-          ->save();
+        ->set('folder_path', $form_state['values']['folder_path'])
+        ->save();
   }
+
 }
