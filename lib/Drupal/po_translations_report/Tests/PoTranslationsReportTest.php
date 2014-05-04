449e7736223e6caaa891d07e595b25a3a4bd1149
izus <abouljamal.ismaeil@gmail.com>
2014-05-04 21:08:18 +0200

    Add simpletest test for config form.

--- lib/Drupal/po_translations_report/Tests/PoTranslationsReportTest.php
+++ lib/Drupal/po_translations_report/Tests/PoTranslationsReportTest.php
@@ -16,22 +16,48 @@ class PoTranslationsReportTest extends WebTestBase {
 
   public static function getInfo() {
     return array(
-      'name' => 'po_translations_report functionality',
+      'name' => 'Po Translations Report functionality',
       'description' => 'Test Unit for module po_translations_report.',
-      'group' => 'Other',
+      'group' => 'Po Translations Report',
     );
   }
 
-  function setUp() {
-    parent::setUp();
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('po_translations_report');
+
+  /**
+   * Tests po_translations_report results.
+   */
+  function testPoTranslationsReportResults() {
+    $this->pass('pass');
   }
 
   /**
-   * Tests po_translations_report functionality.
+   * Tests Admin form.
    */
-  function testpo_translations_report() {
-    //Check that the basic functions of module po_translations_report.
-    $this->assertEqual(TRUE, TRUE, 'Test Unit Generated via Console.');
+  function testPoTranslationsReportAdminForm() {
+     // Create user with 'access po translations report' permission.
+    $access_user = $this->drupalCreateUser(array('access po translations report', 'access administration pages'));
+    $this->drupalLogin($access_user);
+    $this->drupalPostForm('po_translations_report/settings/PoTranslationsReportAdmin', array(
+      'folder_path' => $this->getDataPath(),
+        ),
+        t('Save configuration')
+        );
+    $this->assertText(t('The configuration options have been saved.'), 'Configure folder path');
+  }
+
+  /**
+   * Gets data folder path that contains po test files.
+   */
+  function getDataPath() {
+    $module_path = drupal_get_path('module', 'po_translations_report');
+    $data_sub_path = '/lib/Drupal/po_translations_report/Tests/data';
+    return DRUPAL_ROOT . '/' . $module_path . $data_sub_path;
   }
 
 }
