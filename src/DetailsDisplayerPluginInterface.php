eb6e93a4201c52fefd28d11afaf6a69c4c477ada
izus <abouljamal.ismaeil@gmail.com>
2015-10-10 22:42:59 +0200

    Add new plugin type for details display.

---
+++ src/DetailsDisplayerPluginInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\po_translations_report;
+
+use Drupal\Core\Plugin\PluginFormInterface;
+use Drupal\Component\Plugin\ConfigurablePluginInterface;
+
+/**
+ * Provides an interface for a plugin that displays results.
+ *
+ * @ingroup plugin_api
+ */
+interface DetailsDisplayerPluginInterface extends PluginFormInterface, ConfigurablePluginInterface {
+
+  /**
+   * Extract method.
+   *
+   * @param $results
+   *   array of results to display.
+   */
+  public function display($results);
+
+}
